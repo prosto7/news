8f0936124be3b8ffe397e50ab86daba66ab6627b
Roman Khrapov <roman1199@mail.ru>
2022-01-24 20:05:49 +0700

    fix model News

--- app/Models/News.php
+++ app/Models/News.php
@@ -20,13 +20,5 @@ class News extends Model
         return $this->belongsTo(City::class);
     }
 
-    public function favourite(){
-        $user = 1;
-        return $this->belongsTo(NewsUser::class,'id','news_id')->where('user_id',$user);
-    }
-
-    public function like() {
-        return $this->favourite()->selectRaw('news_id,count(*) as count')->groupBy('news_id');
-    }
 
 }
